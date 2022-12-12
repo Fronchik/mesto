e5b0a31f0956885a70fbc1ce7f7e988da02eca13
Anna Nazareva <A.V.Nazareva@yandex.ru>
2022-12-13 02:03:44 +0300

    js: load initial cards, like/unlike, delete cards

--- script/script.js
+++ script/script.js
@@ -31,3 +31,66 @@ const formSubmitHandler = (evt) => {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.picture');
+
+
+
+const handleLikeClick = (e) => {
+  e.target.classList.toggle("picture__button_active");
+}
+
+const handleDeleteClick = (e) => {
+  e.target.parentElement.remove();
+}
+
+const createCard = (title, link) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.picture__name').textContent = title;
+  const cardImg = cardElement.querySelector('.picture__image');
+  cardImg.src = link;
+  cardImg.alt = title;
+  cardElement.querySelector('.picture__button').addEventListener('click', handleLikeClick);
+  cardElement.querySelector('.picture__basket').addEventListener('click', handleDeleteClick);
+  return cardElement;
+}
+
+const cardList = document.querySelector('.pictures__list');
+
+const loadCards = (cardItems) => {
+  cardItems.forEach(item => {
+    const card = createCard(item.name, item.link);
+    cardList.append(card);
+  });
+}
+
+loadCards(initialCards);
+
+
